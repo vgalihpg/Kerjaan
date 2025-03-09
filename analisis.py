857f64e85b7e9b1d283db70b7a0dca8c79b557fd
vgalihpg <vgalihpg03@gmail.com>
2025-03-10 02:51:04 +0700

    Update analisis.py

--- analisis.py
+++ analisis.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 st.title("Dashboard Analisis Data E-Commerce") 
 st.subheader("Pertanyaan 1: Produk apa yang menjadi penjualan terbesar berdasarkan total harga?") 
-st.image("C:/Users/vgali/Pertanyaan_1.png") 
+st.image("https://github.com/vgalihpg/Kerjaan/blob/main/Pertanyaan_1.png") 
 st.subheader("Pertanyaan 2: Produk manakah yang memiliki rating terbaik dan review terbanyak?") 
 st.image("C:/Users/vgali/Pertanyaan_2.png") 
