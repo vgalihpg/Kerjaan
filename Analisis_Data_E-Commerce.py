0adf5656b6ddf4b2f4ce17d182e98cefc77efae1
vgalihpg <vgalihpg03@gmail.com>
2025-03-10 23:20:08 +0700

    Update Analisis_Data_E-Commerce.py

--- Analisis_Data_E-Commerce.py
+++ Analisis_Data_E-Commerce.py
@@ -10,12 +10,12 @@ option = st.radio("Pilih analisis yang ingin ditampilkan", ("Analisis Penjualan"
 
 if option == "Analisis Penjualan":
     st.subheader("Pertanyaan 1: Produk apa yang menjadi penjualan terbesar berdasarkan total harga?")
-    st.image("C:/Users/vgali/Pertanyaan_1.png")
+    st.image("https://raw.githubusercontent.com/vgalihpg/Kerjaan/refs/heads/main/Pertanyaan_1.png")
     with st.expander("Lihat penjelasan plot"):
         st.text("Plot di atas dibuat berdasarkan hasil penjualan produk-produk E-Commerce dalam rentang tahun 2016 sampai 2018. Produk dengan ID 'bb50f2...' memiliki total harga tertinggi, menunjukkan bahwa produk ini adalah yang paling menguntungkan dalam hal nilai transaksi selama rentang tahun tersebut.")
 
 elif option == "Analisis Rating & Review":
     st.subheader("Pertanyaan 2: Produk manakah yang memiliki rating terbaik dan review terbanyak?")
-    st.image("C:/Users/vgali/Pertanyaan_2.png")
+    st.image("https://raw.githubusercontent.com/vgalihpg/Kerjaan/refs/heads/main/Pertanyaan_2.png")
     with st.expander("Lihat penjelasan plot"):
         st.text("Plot di atas dibuat berdasarkan review pelanggan terhadap suatu produk yang sudah direview paling sedikit lima belas kali terhitung dari tahun 2016 hingga 2018. Produk dengan ID '37eb69...' memiliki rating tertinggi, yang berarti para pelanggan sangat puas dengan produk ini sepanjang rentang tahun tersebut.")
